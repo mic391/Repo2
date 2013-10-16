f3f410f1f35d47e32c82057eb66311f9b384fbeb
michal <michal@localhost.localdomain>
2013-10-16 22:03:16 +0200

    1st com

---
+++ miniprojekt.cpp
@@ -0,0 +1,73 @@
+	
+
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+     
+     
+using namespace std;     
+  
+global char spisdowodow[100];
+global char *dowod_wsk;
+dowod_wsk = & spisdowodow;
+  
+    
+class pojazd {
+
+public:		 
+		 pojazd(): 
+		 
+		
+		 
+		 {
+			 
+			 cout << "podaj nr rejestracyjny" << endl;
+			 cin >> dowodrj; 
+			 cout << "podaj rodzaj pojazdu" << endl;
+			 cin >> rodzaj;
+			
+		}
+		virtual void stworz () = 0;
+     {
+		 
+	 }
+     
+     
+        virtual ~pojazd()
+     
+     
+
+class motor : public pojazd {
+	
+	void stworz () {
+		
+		
+	}
+	
+	
+}
+
+
+class samochod : public pojazd {
+	
+	void stworz () {
+	}
+}
+ 
+     
+     
+class TIR : public pojazd {
+	
+	void stworz () {
+	}
+}
+     
+
+    
+    int main(int argc, char* argv[]) {
+     
+     
+     
+        return 0;
+    }
+
