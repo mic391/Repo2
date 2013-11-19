6e75b307ec59ede158b1c5a795a64f0995ec1468
michal <michal@localhost.localdomain>
2013-11-19 19:15:51 +0100

    1st Stage

--- exercise_01/ex_1.cpp
+++ exercise_01/ex_1.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string> 
+
+
 
 int main(int argc, char **argv)
 {
+	std::string wynik(argv[1]);
+	std::cout << argv[0] << std::endl;
+	std::cout << argv[1] << std::endl;
+	if(wynik=="100") 
+	{
+		std::cout << "you have a perfect score!" << std::endl;
+		
+	}
+	
 	
 	return 0;
 }
