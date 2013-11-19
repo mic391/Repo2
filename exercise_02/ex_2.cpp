15edce86a5b4782ed5dc6b9898e83dbc2df28f17
michal <michal@localhost.localdomain>
2013-11-19 22:48:38 +0100

    Makefile (ex2)

---
+++ exercise_02/ex_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+void rysuj(const char *w)
+
+	{
+		
+		
+		while(*w){
+		for(int i=0;i>2;i++){
+		{ std::cout <<*(w++) << " - "; }
+			}
+		std::cout << std::endl;
+		}
+	}
+
+int main(int argc, char** argv)
+{
+    void char tablica[3][3]
+    rysuj(tablica);    
+    
+    
+    
+    return 0;
+}
+
