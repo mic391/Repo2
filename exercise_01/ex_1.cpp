4f6a91ad0829b60743556b62eaa22ba1b369934e
Attu <attu@archie>
2013-11-19 21:28:06 +0100

    little bit nicer code

--- exercise_01/ex_1.cpp
+++ exercise_01/ex_1.cpp
@@ -1,55 +1,28 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	int numberString;
-	std::string wynik(argv[1]);
-	std::cout << argv[0] << std::endl;
-	std::cout << argv[1] << std::endl; //TODO: what if there is no 1st arg?
-	if(wynik=="100")
-	{
-		std::cout << "you have a perfect score!" << std::endl;
-
-	}
-
-	/// string to number conversion
-	 std::stringstream ss;
-     ss<<wynik;
-     ss>>numberString;
-     ss.str("");
-     ss.clear();
-
-
-
-	if(numberString>=90 && numberString<=100)
-	{
-		std::cout << "you scored an A!" << std::endl;
-
-	}
-	else if(numberString>=80 && numberString<=89)
-
-	{
-		std::cout << "you scored an B!" << std::endl;
-	}
-
-	else if(numberString>=70 && numberString<=79)
-
-	{
-		std::cout << "you scored an C!" << std::endl;
-	}
-	else if(numberString>=60 && numberString<=69)
-
-	{
-		std::cout << "you scored an D!" << std::endl;
-	}
-	else if(numberString>=0 && numberString<=59)
-
-	{
-		std::cout << "you scored an F! Learn idiot" << std::endl;
-	}
-
-	return 0;
+    if(argc != 2) {
+        std::cout << "Give one score!" << std::endl;
+        return 1;
+    }
+    // string to int conversion: std::stoi is in c++11
+    int score = std::stoi(std::string(argv[1]));
+    if (score == 100) {
+        std::cout << "you have a perfect score!" << std::endl;
+    }
+    if (score >= 90 && score <= 100) {
+        std::cout << "you scored an A!" << std::endl;
+    } else if (score >= 80 && score <= 89) {
+        std::cout << "you scored an B!" << std::endl;
+    } else if (score >= 70 && score <= 79) {
+        std::cout << "you scored an C!" << std::endl;
+    } else if (score >= 60 && score <= 69) {
+        std::cout << "you scored an D!" << std::endl;
+    } else if (score >= 0 && score <= 59) {
+        std::cout << "you scored an F! Learn idiot" << std::endl;
+    }
+    return 0;
 }
 
