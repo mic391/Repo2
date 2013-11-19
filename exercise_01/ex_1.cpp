ca0f470f2c893095e4014b3818fb8e8d91f72a1e
michal <michal@localhost.localdomain>
2013-11-19 20:34:52 +0100

    2nd stage attempt:1

--- exercise_01/ex_1.cpp
+++ exercise_01/ex_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string> 
-
-
+#include <sstream>
 
 int main(int argc, char **argv)
 {
+	int numberString;
 	std::string wynik(argv[1]);
 	std::cout << argv[0] << std::endl;
 	std::cout << argv[1] << std::endl;
@@ -14,6 +14,19 @@ int main(int argc, char **argv)
 		
 	}
 	
+	/// string to number conversion
+	 stringstream ss;
+     ss<<wynik;
+     ss>>numberString; 
+     ss.str(""); 
+     ss.clear(); 
+     
+     
+	if(numberString>=90 && numberString<=100) 
+	{
+		std::cout << "you scored an A!" << std::endl;
+		
+	}
 	
 	return 0;
 }
