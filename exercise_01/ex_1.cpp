0fbea0dd2a994dea7db696ca69b9eb3ef23d7f99
michal <michal@localhost.localdomain>
2013-11-19 20:44:08 +0100

    Done 1

--- exercise_01/ex_1.cpp
+++ exercise_01/ex_1.cpp
@@ -15,17 +15,39 @@ int main(int argc, char **argv)
 	}
 	
 	/// string to number conversion
-	 stringstream ss;
+	 std::stringstream ss;
      ss<<wynik;
      ss>>numberString; 
      ss.str(""); 
      ss.clear(); 
      
      
+     
 	if(numberString>=90 && numberString<=100) 
 	{
 		std::cout << "you scored an A!" << std::endl;
 		
+	}
+	else if(numberString>=80 && numberString<=89)
+	
+	{
+		std::cout << "you scored an B!" << std::endl;
+	}
+	
+	else if(numberString>=70 && numberString<=79)
+	
+	{
+		std::cout << "you scored an C!" << std::endl;
+	}
+	else if(numberString>=60 && numberString<=69)
+	
+	{
+		std::cout << "you scored an D!" << std::endl;
+	}
+	else if(numberString>=0 && numberString<=59)
+	
+	{
+		std::cout << "you scored an F! Learn idiot" << std::endl;
 	}
 	
 	return 0;
