31a9d00544e9cd4042feeaa07c1f0b7d47291687
michal <michal@localhost.localdomain>
2013-11-20 14:37:25 +0100

    Just checking

--- exercise_02/ex_2.cpp
+++ exercise_02/ex_2.cpp
@@ -7,7 +7,7 @@ void rysuj(const char *w)
 		
 		std::cout << std::endl;
 		while(*w){
-			for(int i=0;i>2;i++){
+			for(int i=0;i<2;i++){
 			{ std::cout <<*(w++) << " - "; }
 			}
 		std::cout << std::endl;
@@ -16,7 +16,7 @@ void rysuj(const char *w)
 
 int main(int argc, char** argv)
 {
-    void char tablica[2][2] = {"x","o","x","o","x","o","x","o","x",};
+    void char tablica[2][2] = {"x","o","x","o","x","o","x","o","x"};
     
     rysuj(tablica);    
     
