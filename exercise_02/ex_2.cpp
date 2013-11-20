7841bfa49de9bf4ade7dd9b0de9678a2fed91cfd
michal <michal@localhost.localdomain>
2013-11-20 14:34:58 +0100

    testy 2

--- exercise_02/ex_2.cpp
+++ exercise_02/ex_2.cpp
@@ -5,10 +5,10 @@ void rysuj(const char *w)
 
 	{
 		
-		
+		std::cout << std::endl;
 		while(*w){
-		for(int i=0;i>2;i++){
-		{ std::cout <<*(w++) << " - "; }
+			for(int i=0;i>2;i++){
+			{ std::cout <<*(w++) << " - "; }
 			}
 		std::cout << std::endl;
 		}
@@ -16,7 +16,8 @@ void rysuj(const char *w)
 
 int main(int argc, char** argv)
 {
-    void char tablica[3][3]
+    void char tablica[2][2] = {"x","o","x","o","x","o","x","o","x",};
+    
     rysuj(tablica);    
     
     
