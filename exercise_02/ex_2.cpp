939b77e65e06e1b0f0f9520b3302ba225b7ed3c9
Attu <attu@archie>
2013-11-20 21:54:45 +0100

    astyle

--- exercise_02/ex_2.cpp
+++ exercise_02/ex_2.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <string>
 
-void rysuj(const char *w)
+void rysuj(const char* w)
 
-	{
-		
-		std::cout << std::endl;
-		while(*w){
-//			for(int i=0;i<2;i++){
-			{ std::cout <<*(w++) << " - "; }
-//			}
-		std::cout << std::endl;
-		}
-	}
+{
+  std::cout << std::endl;
+  while (*w) {
+    //      for(int i=0;i<2;i++){
+    { std::cout << *(w++) << " - "; }
+    //      }
+    std::cout << std::endl;
+  }
+}
 
 int main(int argc, char* argv)
 {
-    char tablica[2][2] = {"xoxoxoxox"};
-    
-    rysuj(tablica);    
-         
-    return 0;
+  char tablica[2][2] = {"xoxoxoxox"};
+  rysuj(tablica);
+  return 0;
 }
