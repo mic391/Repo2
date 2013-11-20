85a5d22a5e5ad4bd1d6934309ab9589edc0f4b27
michal <michal@localhost.localdomain>
2013-11-20 14:44:57 +0100

    proby

--- exercise_02/ex_2.cpp
+++ exercise_02/ex_2.cpp
@@ -6,22 +6,19 @@ void rysuj(const char *w)
 	{
 		
 		std::cout << std::endl;
-		while(*w){
-			for(int i=0;i<2;i++){
+		while(**w){
+//			for(int i=0;i<2;i++){
 			{ std::cout <<*(w++) << " - "; }
-			}
+//			}
 		std::cout << std::endl;
 		}
 	}
 
-int main(int argc, char** argv)
+int main(int argc, char* argv)
 {
-    void char tablica[2][2] = {"x","o","x","o","x","o","x","o","x"};
+    char tablica[2][2] = {"x","o","x","o","x","o","x","o","x"};
     
     rysuj(tablica);    
-    
-    
-    
+         
     return 0;
 }
-
