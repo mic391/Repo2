aaaf8458438f77e64db66773011b9a72c475eae0
Attu <attu@archie>
2013-11-23 17:44:01 +0100

    canCreateTicTacToeGame

---
+++ exercise_02/inc/tictactoe/game.hpp
@@ -0,0 +1,13 @@
+#ifndef _TICTACTOE_GAME_HPP
+#define _TICTACTOE_GAME_HPP
+
+namespace tictactoe
+{
+
+class Game
+{
+};
+
+}
+
+#endif
--- exercise_02/test/TestTicTacToe.cpp
+++ exercise_02/test/TestTicTacToe.cpp
@@ -1,10 +1,13 @@
 #include <gtest/gtest.h>
+#include "tictactoe/game.hpp"
 
 class TicTacToeTest : public testing::Test
 {
 };
 
-TEST_F(TicTacToeTest, testNothing)
+TEST_F(TicTacToeTest, canCreateTicTacToeGame)
 {
+  tictactoe::Game game;
+  (void)game;
 }
 
